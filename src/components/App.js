7408824022bca960e391ace31b14dfb691473491
Greg <you@example.com>
2019-05-22 13:15:04 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+import UserList from './UserList';
+
+const App = () => {
+    const [resource, setResource] = useState('posts');
+
+    return (
+        <div className="ui container">
+        <UserList />
+            <div>
+                <button
+                    onClick={() => setResource('posts')}
+                    className="ui button">
+                    Posts
+                    </button>
+                <button
+                    onClick={() => setResource('todos')}
+                    className="ui button">
+                    Todos
+                </button>
+            </div>
+            <ResourceList resource={resource} />
+        </div>
+    );
+}
+
+export default App;
+
